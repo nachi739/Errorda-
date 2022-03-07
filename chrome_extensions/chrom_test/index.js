2429d0fcb848ce9201dbc2fb27f2f9aeba3cc0ae
Yuichi Namiki <namiki081025@gmail.com>
2022-03-07 12:23:37 +0900

    edit

--- chrome_extensions/chrom_test/index.js
+++ chrome_extensions/chrom_test/index.js
@@ -1,27 +1,28 @@
 window.addEventListener('load', (event) => { //webページを読み込む際にアクションを行う
-    const getButton = document.getElementById('get-js')
-    const postText = document.getElementById('post-text')
-    const postButton = document.getElementById('post-js')
-    const resHtml = document.getElementById('res-js')
-    let time = new Date()
-    let search_error;
-      axios.get('http://127.0.0.1:3000/api/v1/user/stumblings/searching').then(res => search_error = res.data); //
+const getButton = document.getElementById('get-js')
+const postText = document.getElementById('post-text')
+const postButton = document.getElementById('post-js')
+const resHtml = document.getElementById('res-js')
+let searching;
+let end_time = new Date;
 
-    //console.log(search_error.id)
+
+    axios.get('http://127.0.0.1:3000/api/v1/user/stumblings/searching').then(res => searching = res.data);
+
+    console.log(searching);
+
     getButton.addEventListener('click', event => {
-
-      let frontSideTime= new Date()
-      console.log(frontSideTime)
-      axios.put(`http://127.0.0.1:3000/api/v1/user/stumblings/${search_error.id}`).then(res =>
-      resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))
-
+      this.axios.get(`http://127.0.0.1:3000/api/v1/user/stumblings/${searching.id}`, {
+        end_time
+      }).then(res => resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))
     });
 
     postButton.addEventListener('click', event => { //buttonクリック時に行うアクション
-      console.log(search_error)
       axios.post('http://127.0.0.1:3000/api/v1/user/stumblings', {
         search_key: postText.value,
       }).then(res => resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))
     });
-
   });
+
+
+
