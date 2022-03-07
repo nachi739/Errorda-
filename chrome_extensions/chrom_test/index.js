5100de60f395f522659f4b807f8982fa9259a5aa
Yuichi Namiki <namiki081025@gmail.com>
2022-03-07 20:13:55 +0900

    end_time

--- chrome_extensions/chrom_test/index.js
+++ chrome_extensions/chrom_test/index.js
@@ -1,20 +1,18 @@
 window.addEventListener('load', (event) => { //webページを読み込む際にアクションを行う
 const getButton = document.getElementById('get-js')
+const putButton = document.getElementById('put-js')
 const postText = document.getElementById('post-text')
 const postButton = document.getElementById('post-js')
 const resHtml = document.getElementById('res-js')
 let searching;
-let end_time = new Date;
+const time =new Date()
 
+console.log(time)
 
     axios.get('http://127.0.0.1:3000/api/v1/user/stumblings/searching').then(res => searching = res.data);
 
-    console.log(searching);
-
     getButton.addEventListener('click', event => {
-      this.axios.get(`http://127.0.0.1:3000/api/v1/user/stumblings/${searching.id}`, {
-        end_time
-      }).then(res => resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))
+      axios.get(`http://127.0.0.1:3000/api/v1/user/stumblings/${searching.id}`).then(res => resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))
     });
 
     postButton.addEventListener('click', event => { //buttonクリック時に行うアクション
