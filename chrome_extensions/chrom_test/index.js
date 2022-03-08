65c4538632de40a7b2a07de9f208a340077e995d
Yuichi Namiki <namiki081025@gmail.com>
2022-03-08 21:21:02 +0900

    chrome

--- chrome_extensions/chrom_test/index.js
+++ chrome_extensions/chrom_test/index.js
@@ -1,19 +1,16 @@
 window.addEventListener('load', (event) => { //webページを読み込む際にアクションを行う
 const getButton = document.getElementById('get-js')
-const putButton = document.getElementById('put-js')
 const postText = document.getElementById('post-text')
 const postButton = document.getElementById('post-js')
 const resHtml = document.getElementById('res-js')
 let searching;
-const time =new Date()
 
-console.log(time)
 
     axios.get('http://127.0.0.1:3000/api/v1/user/stumblings/searching').then(res => searching = res.data);
 
     getButton.addEventListener('click', event => {
-      axios.get(`http://127.0.0.1:3000/api/v1/user/stumblings/${searching.id}`).then(res => resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))
-    });
+      axios.get(`http://127.0.0.1:3000/api/v1/user/stumblings/${searching.id}`)
+      .then(res =>resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))});
 
     postButton.addEventListener('click', event => { //buttonクリック時に行うアクション
       axios.post('http://127.0.0.1:3000/api/v1/user/stumblings', {
---
+++ chrome_extensions/js/script.js
@@ -0,0 +1,23 @@
+
+window.addEventListener('load', (event) => { //webページを読み込む際にアクションを行う
+    const getButton = document.getElementById('get-js')
+    const postText = document.getElementById('post-text')
+    const postButton = document.getElementById('post-js')
+    const resHtml = document.getElementById('res-js')
+    let searching;
+
+        axios.get('http://127.0.0.1:3000/api/v1/user/stumblings/searching/').then(res => searching = res.data);
+
+        getButton.addEventListener('click', event => {
+            axios.get(`http://127.0.0.1:3000/api/v1/user/stumblings/${searching.id}`)
+            .then(res =>resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))});
+
+        postButton.addEventListener('click', event => { //buttonクリック時に行うアクション
+            axios.post('http://127.0.0.1:3000/api/v1/user/stumblings', {
+                search_key: postText.value,
+            }).then(res => resHtml.insertAdjacentText('afterbegin', JSON.stringify(res.data)))
+        });
+    });
+
+
+
