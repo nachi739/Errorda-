e72127c59d6623719698dcde220b4cc36b144676
Yuichi Namiki <namiki081025@gmail.com>
2022-03-31 13:51:21 +0900

    [update]chromeインデント修正

--- chrome_extensions/background.js
+++ chrome_extensions/background.js
@@ -1,86 +1,126 @@
-//アイコンクリック時に動作するため
 window.onload = function () {
-    //ローカルストレージの値を取得
-    chrome.storage.local.get(['key'], function(result) {
-        //保存されている名前の確認のため
-        console.log(result.key);
-        //登録されているかの有無の確認 初回のみ実行
-        if(result.key === null) {
-            //アラートの表示 現段階では初回表示のみ
-            const name = prompt('ユーザー名を登録して下さい', 'error');
-            //値をクロームストレージに保存
-            chrome.storage.local.set({key: name}, function() {
-                //保存された値の確認
-                console.log(name);
-            });
-        }
-        //popup.htmlで表示するための記述
-        const user = document.getElementById('user');
-        //そのまま文字として出力するため
-        user.textContent = result.key;
-        //ユーザー名をポストする
-        const params = {
-            method: 'POST',
-            headers: {'Content-type': 'application/json;charset=utf-8'},
-            body: JSON.stringify({name: result.key})
-        };
-        fetch("http://127.0.0.1:3000/api/v1/user/stumblings/", params)
-        .then(res => res.json());
-
-
-    const searched = document.getElementById('searched');
-    const searching = document.getElementById('searching');
-    if(searched.style.visibility = "hidden"){
-        searched.style.visibility = "visible";
-        searching.style.visibility = "hidden";
+  //開発用
+  const baseUrl = "http://127.0.0.1:3000";
+  //プロダクト用
+  //const baseUrl ="https://errorda-backend.herokuapp.com/"
+  //
+  const url = `${baseUrl}/api/v1/user/stumblings/searching`;
+  //検索窓
+  const searched = document.getElementById('searched');
+  //検索中
+  const searching = document.getElementById('searching');
+  //一覧画面に遷移するためのボタン
+  const  listButton = document.getElementById('list-js');
+  //一覧画面に遷移するURL
+  const listURL = `${baseUrl}/api/v1/user/stumblings/`;
+  //検索時のテキスト
+  const postText = document.getElementById('post-text');
+  //検索開始ボタン
+  const postButton = document.getElementById('post-js');
+  //
+  const postUrl = `${baseUrl}/api/v1/user/stumblings/`;
+	//
+	const postUser = `${baseUrl}/api/v1/users/`;
+  //
+  const getButton = document.getElementById('get-js');
+  //
+  //const getUrl = `${baseUrl}/api/v1/user/stumblings/searching`;
+  //chromeストレージの値を取得
+  chrome.storage.local.get(['key'], function(result) {
+    //保存されている名前の確認のため
+    console.log(result.key);
+    //登録されているかの有無の確認 初回のみ実行
+    if(result.key === null) {
+        //アラートの表示 現段階では初回表示のみ
+        const name = prompt('ユーザー名を登録して下さい', 'error');
+        //値をクロームストレージに保存
+        chrome.storage.local.set({key: name}, function() {
+            //保存された値の確認
+            console.log(name);
+        });
     }
-    const baseUrl = "http://127.0.0.1:3000"; //開発用
-    //const baseUrl ="https://errorda-backend.herokuapp.com/" //プロダクト用
-    const url = `${baseUrl}/api/v1/user/stumblings/searching`;
-    fetch(url ,params) //ユーザ情報を認識する
-
-        .then(res => res.json())
-        .then(function (jsonData) {
-            if (jsonData) { //検索窓の表示・非表示
-                searched.style.visibility = "hidden";
-                searching.style.visibility = "visible";
-            }
-            console.log(jsonData); //検証でどのエラーを取ってきているか確認
-            console.log(jsonData.id); //検証でidを確認するため
-        })
-    const  listButton = document.getElementById('list-js');
-    listButton.onclick = function () { //一覧画面遷移
-        const listURL = `${baseUrl}/api/v1/user/stumblings/`;
-        chrome.tabs.create({ url: listURL });
+    //popup.htmlで表示するための記述
+    const user = document.getElementById('user');
+    //そのまま文字として出力するため
+    user.textContent = result.key;
+    //ユーザー名をポストする
+    const params = {
+      method: 'POST',
+      headers: {'Content-type': 'application/json;charset=utf-8'},
+      body: JSON.stringify({name: result.key})
     };
-    const postText = document.getElementById('post-text');
-    const postButton = document.getElementById('post-js');
-    const postUrl = `${baseUrl}/api/v1/user/stumblings/`;
-    postButton.onclick = res => { //新規作成は確認　search_keyにテキスト保存確認
-        const params = {
-            method: 'POST',
-            headers: {'Content-type': 'application/json;charset=utf-8'},
-            body: JSON.stringify({name: result.key, search_key: postText.value})
-        };
-        fetch(postUrl, params)
+    fetch(postUrl, params)
+      .then(res => res.json())
+			.then(function (jsonData){
+				console.log(jsonData);
+			})
+			// fetch(url, params)
+      // .then(res => res.json())
+			// .then(function (jsonData){
+			// 	console.log(jsonData);
+			// })
+    //
+    if(searched.style.visibility = "hidden"){
+      //
+      searched.style.visibility = "visible";
+      //
+      searching.style.visibility = "hidden";
+    }
+    //ユーザ情報を認識する
+    fetch(url ,params)
+      //
+      .then(res => res.json())
+      //
+      .then(function (jsonData) {
+        //検索窓の表示・非表示
+        if (jsonData) {
+          searched.style.visibility = "hidden";
+          searching.style.visibility = "visible";
+        }
+        //検証でどのエラーを取ってきているか確認
+        console.log(jsonData);
+        //検証でidを確認するため
+        console.log(jsonData.id);
+      })
+    //一覧画面遷移
+    listButton.onclick = function () {
+      //
+      chrome.tabs.create({ url: listURL });
+    };
+    //新規作成は確認　search_keyにテキスト保存確認
+    postButton.onclick = res => {
+      const params = {
+          method: 'POST',
+          headers: {'Content-type': 'application/json;charset=utf-8'},
+          body: JSON.stringify({name: result.key, search_key: postText.value})
+      };
+      fetch(postUrl, params)
         .then(res => res.json());
+        //
         const searchURL = 'https://www.google.com/search?q=' + encodeURIComponent(postText.value);
-        chrome.tabs.create({ url: searchURL }); //新規タブでsearch_keyの内容を検索
+        //新規タブでsearch_keyの内容を検索
+        chrome.tabs.create({ url: searchURL });
     }
-    const getButton = document.getElementById('get-js');
-    const getUrl = `${baseUrl}/api/v1/user/stumblings/searching`; //json形式でend_timeがnullのものを取得
     getButton.onclick = res => {
-    fetch(getUrl, params)
-        //.then(function (res) {
-        //    return res.json();
-        //})
-        .then(res => res.json()) //上記のコードを簡略化
-        .then(function (jsonData) {
-            const getUrlId = `${baseUrl}/api/v1/user/stumblings/${jsonData.id}`; //jsonDataに入っているidを取得
-            console.log(jsonData);
-            chrome.tabs.create({ url: getUrlId });
-        })
-
+			const params = {
+				method: 'POST',
+				headers: {'Content-type': 'application/json;charset=utf-8'},
+				body: JSON.stringify({name: result.key})
+			};
+      fetch(url, params)
+          //.then(function (res) {
+          //    return res.json();
+          //})
+          //上記のコードを簡略化
+          .then(res => res.json())
+          .then(function (jsonData) {
+              //jsonDataに入っているidを取得
+              const getUrl = `${baseUrl}/api/v1/user/stumblings/${jsonData.id}`;
+              //
+              console.log(jsonData);
+              //
+              chrome.tabs.create({ url: getUrl });
+          })
     }
-});
-};
+  })
+}
